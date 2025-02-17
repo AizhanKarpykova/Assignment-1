4c9fa90de34274c8a9cc07246ee0dbb5641b1ad1
AizhanKarpykova <ak11293@nyu.edu>
2025-02-17 20:40:48 +0400

    added dots to slider

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
-
-const images = ["img/IMG_1.jpeg", "img/IMG_2.jpeg", "img/IMG_3.jpeg", "img/IMG_4.jpeg", "img/IMG_5.jpeg"];
-let index = 0;
-const imgElement = document.getElementById("slider-image");
-document.getElementById("prev").addEventListener("click", () => {
+const images = [
+    "img/IMG_1.jpeg",
+    "img/IMG_2.jpeg",
+    "img/IMG_3.jpeg",
+    "img/IMG_4.jpeg",
+    "img/IMG_5.jpeg",
+  ];
+  let index = 0;
+  const imgElement = document.getElementById("slider-image");
+  const dotsContainer = document.getElementById("dots-container");
+  
+  // Function to create dots
+  function createDots() {
+    images.forEach((_, i) => {
+      const dot = document.createElement("span");
+      dot.classList.add("dot");
+      if (i === 0) dot.classList.add("active");
+      dot.addEventListener("click", () => updateSlider(i));
+      dotsContainer.appendChild(dot);
+    });
+  }
+  
+  // Function to update the slider
+  function updateSlider(newIndex) {
+    index = newIndex;
+    imgElement.src = images[index];
+    updateDots();
+  }
+  
+  // Function to update the active dot
+  function updateDots() {
+    const dots = document.querySelectorAll(".dot");
+    dots.forEach((dot, i) => {
+      if (i === index) {
+        dot.classList.add("active");
+      } else {
+        dot.classList.remove("active");
+      }
+    });
+  }
+  
+  // Event listeners for prev and next buttons
+  document.getElementById("prev").addEventListener("click", () => {
     index = (index - 1 + images.length) % images.length;
-    imgElement.src = images[index];
-});
-document.getElementById("next").addEventListener("click", () => {
+    updateSlider(index);
+  });
+  
+  document.getElementById("next").addEventListener("click", () => {
     index = (index + 1) % images.length;
-    imgElement.src = images[index];
-});
+    updateSlider(index);
+  });
+  
+  // Initialize dots
+  createDots();
